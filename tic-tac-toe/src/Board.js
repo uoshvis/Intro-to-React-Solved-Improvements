ba5536bad301991d8889a184eae8161dc4360890
uoshvis <uoshvis@gmail.com>
2022-02-13 20:25:19 +0200

    rewrite Board to use two loops

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -13,23 +13,22 @@ class Board extends React.Component {
     }
   
     render() {
+        let boardSquares = []
+        for (var row = 0; row < 3; row++){
+            let boardRow = []
+            for(var col = 0; col < 3; col++){
+                boardRow.push(
+                    <span key={row*3 + col}>
+                        {this.renderSquare(row*3 + col)}
+                    </span>
+                )
+            }
+            boardSquares.push(<div className='board-row' key={row}>{boardRow}</div>)
+        }
+
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+            {boardSquares}
         </div>
       );
     }
